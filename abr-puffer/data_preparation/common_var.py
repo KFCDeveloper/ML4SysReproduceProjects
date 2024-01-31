cf931376df197598b9c97cd85306fc808fabdc7e
KFCDeveloper <ydaiyang@gmail.com>
2024-01-31 02:39:15 -0600

    update

--- abr-puffer/data_preparation/common_var.py
+++ abr-puffer/data_preparation/common_var.py
@@ -3,4 +3,4 @@ import datetime
 
 class Durration_CON:
     start_date = datetime.date(2020, 9, 27) # 2020, 7, 27; 2020, 9, 27; 2020, 11, 27; 2021, 1, 27;
-    end_date = Durration_CON.end_date
+    end_date = datetime.date(2021, 6, 1)
