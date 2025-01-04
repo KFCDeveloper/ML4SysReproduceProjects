9119adbc65b6d6fe2a08eb9b94b8cad2e953a7e8
Yan <yliangbyhk@gmail.com>
2025-01-03 19:30:43 -0600

    Clean up NetLLM_vp branch by removing unnecessary files

--- networking_envs/data_gen/consts.py
+++
@@ -1,9 +0,0 @@
-class TMType:
-    BIMODAL = "bimodal"
-    GRAVITY = "gravity"
-    CAPACITY_BIMODAL = "capbimodal"
-    EXP = "exp"
-    GAUSSIAN = "gausian"
-    ABILENE = "abilene"
-    GEANT = "geant"
-
--- networking_envs/networking_env/utils/env_selector.py
+++
@@ -1,6 +0,0 @@
-from networking_env.environments.ecmp.history_env import ECMPHistoryEnv
-
- 
-def get_env(props):
-    env = ECMPHistoryEnv(props)
-    return env
